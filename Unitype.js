ec9f7dc51f52c114429453b9328cbd2e440e3778
Vadim Brik <Brik99brik@gmail.com>
2017-02-14 14:57:25 +0600

    Update Unitype.js

--- Unitype.js
+++ Unitype.js
@@ -1,2 +1,2 @@
 'use strict';
-let Unitype = object => object instanceof Object ? object.constructor.name : ({}.toString.call(object)).slice(8, -1).toLowerCase();
+const Unitype = object => object instanceof Object ? object.constructor.name : ({}.toString.call(object)).slice(8, -1).toLowerCase();
